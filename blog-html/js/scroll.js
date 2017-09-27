2f2c8a198b6603730b4466c2899a1149a318dfea
wxiaojia <630993632@qq.com>
2017-09-27 22:38:14 +0800

    主页修改1

--- blog-html/js/scroll.js
+++ blog-html/js/scroll.js
@@ -1,4 +1,9 @@
 $(function(){  
+	var right=$('.middle .right');
+	var right_top=right.offset().top;
+	var header_height=$("header").height();
+	var right_right=screen.width-right.offset().left-right.width()-15;
+
 	$(window).bind("scroll", function(){ 
 	    var top = $(this).scrollTop(); // 当前窗口的滚动距离
 	    if(top>=100){
@@ -19,6 +24,11 @@ $(function(){
 	    	$(".navs a").removeClass("nonav");
 			$(".navs a").addClass("nav");
 	    }
+	    // 右侧
+	    if(top>=right_top){
+	    	right.css({"position":"fixed","margin-top":"-396px","right":right_right+"px"});
+	    }else{
+	    	right.css({"position":"","margin-top":"12px","right":""});
+	    }
 	});
-
 })
