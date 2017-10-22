72c47bf3e0ec0411279528a12de785404c97afb6
风雨雾凇 <602766695@qq.com>
2017-10-23 00:09:35 +0800

    大致完成、todo: 响应式、加载动画

--- src/main/java/cn/niriqiang/blog/BlogApplication.java
+++ src/main/java/cn/niriqiang/blog/BlogApplication.java
@@ -2,9 +2,10 @@ package cn.niriqiang.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
